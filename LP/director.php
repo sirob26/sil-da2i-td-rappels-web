9f58000a14eebe6c15004fc932c020b9ad2cdbcc
Boris <borishenrot@gmail.com>
2018-11-13 10:40:16 +0100

    étape 1

--- LP/director.php
+++ LP/director.php
@@ -3,6 +3,14 @@ function getBlock($file, $data = [])
 {
     require $file;
 }
+
+function getDataBase(){
+    $dsn = 'mysql:host=localhost;dbname=LP_dev;charset=utf8';
+    $result = new PDO($dsn, 'root', '');
+    return $result;
+}
+
+$dbh = getDataBase();
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,11 +22,31 @@ function getBlock($file, $data = [])
     <?php getBlock('header.php'); ?>
     	<main>
     	<h1>Matthew Vaughn</h1>
-    		<?php getBlock('matthewVaughn.php'); ?>
     		
-    		<?php getBlock('matthewVaughnFilmographie.php'); ?>
-    
     	</main>
+    	<article>
+    		<h2>Ses acteurs favoris</h2>
+    		<p>
+    			<?php 
+    			     $reponse = $dbh->query('SELECT * FROM picture WHERE id IN(SELECT idPicture FROM personHasPicture WHERE idPerson = 2)');
+                     $person = $reponse->fetch();
+               
+    			echo ("<figure>
+                	<img src=\"". $person['path']."\" alt=\"Acteur : ".$person['legend']."\">
+                	<figcaption>".$person['legend']."</figcaption>
+                </figure>");
+    			
+    			$reponse = $dbh->query('SELECT * FROM picture WHERE id IN(SELECT idPicture FROM personHasPicture WHERE idPerson = 3)');
+    			$person = $reponse->fetch();
+    			
+    			echo ("<figure>
+                	<img src=\"". $person['path']."\" alt=\"Acteur : ".$person['legend']."\">
+                	<figcaption>".$person['legend']."</figcaption>
+                </figure>");
+    			?>
+            </p>
+    		<p>Acteur deux</p>
+    	</article>
     <?php include 'footer.php';?>
     </body>
 </html>
